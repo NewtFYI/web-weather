eca6e9a04388365e05690178f23c37f14bed5b59
Crash Bandinewt Developer <developer@newt.fyi>
2026-07-12 21:31:22 +0200

    biome lint clean

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// biome-ignore lint/style/noNonNullAssertion: We control the index.html and know that root is there. The fact that getElementById could return undefined, is just because it doesn't know the HTML. This is not a problem worth adding "checking" code
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
