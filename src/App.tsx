54d4f34d6129cbca8908c15886c57cfde007f310
Crash Bandinewt Developer <developer@newt.fyi>
2026-07-14 14:49:56 +0200

    All app changes to the newly added location and weather data changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
-import Hero from "./components/Hero.tsx";
-import LoadingRetry from "./components/LoadingRetry.tsx";
+import Hero from "./components/Hero/Hero.tsx";
+import { LocationHeader } from "./components/LocationHeader/LocationHeader.tsx";
 import { useWeather } from "./hooks/useWeather.ts";
-import type { TempUnit } from "./types/weather.ts";
+import type { TempUnit, WeatherDay, WeatherLocation } from "./types/weather.ts";
 
 function App() {
 	const [selectedUnit, setSelectedUnit] = useState<TempUnit>("C");
 	// TODO remove defaults, should not be necessary
-	const [weatherData, setWeatherData] = useState<{ heroTemp: number; min: number; max: number }>({ heroTemp: 22, min: 18, max: 28 });
+	const [weatherData, setWeatherData] = useState<WeatherDay>({
+		heroTemp: 21,
+		max: 27,
+		min: 20,
+		feelsLike: 22,
+		isDay: true,
+		conditionText: "Hogwash",
+	});
+	const [locationData, setLocationData] = useState<WeatherLocation>({
+		region: "My House",
+		timezoneId: "Africa/Johannesburg",
+		name: "Home",
+	});
 
 	const { status, data, error, refresh } = useWeather("Johannesburg");
 
@@ -16,18 +28,24 @@ function App() {
 			case "C": {
 				// TODO use correct values
 				setWeatherData({
-					heroTemp: data?.current.temp_c ?? 0,
-					max: data?.current.temp_c ?? 0,
-					min: data?.current.temp_c ?? 0,
+					heroTemp: data?.current.heroTemp ?? 0,
+					max: data?.current.max ?? 0,
+					min: data?.current.max ?? 0,
+					feelsLike: data?.current.feelsLike ?? 0,
+					isDay: data?.current.isDay ?? false,
+					conditionText: data?.current.conditionText ?? "",
 				});
 				break;
 			}
 			case "F": {
 				// TODO use correct values
 				setWeatherData({
-					heroTemp: data?.current.temp_f ?? 0,
-					max: data?.current.temp_f ?? 0,
-					min: data?.current.temp_f ?? 0,
+					heroTemp: data?.current.heroTemp ?? 0,
+					max: data?.current.max ?? 0,
+					min: data?.current.min ?? 0,
+					feelsLike: data?.current.feelsLike ?? 0,
+					isDay: data?.current.isDay ?? false,
+					conditionText: data?.current.conditionText ?? "",
 				});
 				break;
 			}
@@ -39,33 +57,42 @@ function App() {
 			// default to C
 			// TODO use correct values
 			setWeatherData({
-				heroTemp: data?.current.temp_c ?? 0,
-				max: data?.current.temp_c ?? 0,
-				min: data?.current.temp_c ?? 0,
+				heroTemp: data?.current.heroTemp ?? 0,
+				max: data?.current.max ?? 0,
+				min: data?.current.max ?? 0,
+				feelsLike: data?.current.feelsLike ?? 0,
+				isDay: data?.current.isDay ?? false,
+				conditionText: data?.current.conditionText ?? "",
+			});
+
+			setLocationData({
+				name: data?.location.name,
+				timezoneId: data?.location.timezoneId,
+				region: data?.location.region,
 			});
 		}
 	}, [data]);
 
-	if (!data) {
-		return <LoadingRetry retry={refresh} status={status} error={error} />;
-	}
+	// if (!data) {
+	// 	return <LoadingRetry retry={refresh} status={status} error={error} />;
+	// }
 
 	// TODO - remove main-container or add styling
 	return (
-		<div className="main-container">
-			<div className="relative z-2 mx-auto flex min-h-screen max-w-270 flex-col pt-10 pb-10">
-				<Hero
-					day={{ conditionText: "cold and miserable", ...weatherData }}
-					unit={selectedUnit}
-					onUnitChange={(unit) => setSelectedUnit(unit)}
-				/>
-				<footer className="mt-auto flex items-center gap-2 pt-2 text-xs text-slate-500">
-					Powered by{" "}
-					<a href="https://www.weatherapi.com/" title="Free Weather API">
-						WeatherAPI.com
-					</a>
-				</footer>
-			</div>
+		<div className="relative mx-auto flex min-h-screen max-w-270 flex-col pt-10 pb-10">
+			<LocationHeader
+				location={locationData}
+				offToday={false}
+				onBackToToday={() => console.log("back")}
+				onSelectCity={() => console.log("onSelectCity")}
+			/>
+			<Hero day={weatherData} unit={selectedUnit} onUnitChange={(unit) => setSelectedUnit(unit)} />
+			<footer className="mt-auto flex items-center gap-1 pt-2 text-xs text-slate-500">
+				Powered by{" "}
+				<a href="https://www.weatherapi.com/" title="Free Weather API">
+					WeatherAPI.com
+				</a>
+			</footer>
 		</div>
 	);
 }
